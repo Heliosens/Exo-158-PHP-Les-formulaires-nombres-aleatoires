db7d18deaf956e9ea7cce55b2a9712c81dc8c165
Sylvie-B <unsolo@hotmail.fr>
2022-01-18 09:15:34 +0100

    method post

--- form.php
+++ form.php
@@ -1,10 +1,6 @@
 <?php
 
-function aleaNbr ($x, $y){
-    echo rand($x, $y);
-}
-
 if(isset($_POST['minNbr']) && isset($_POST['maxNbr'])){
-    aleaNbr($_POST['minNbr'], $_POST['maxNbr']);
+    echo "nombre aleatoire entre " . $_POST['minNbr'] . " et " . $_POST['maxNbr'] . " : " . rand($_POST['minNbr'], $_POST['maxNbr']);
 }
 
